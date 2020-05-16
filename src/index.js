88e958f34f7aadc6581f642a4b40bc88cf2cb354
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-05-16 17:10:37 -0300

    :rocket: initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Main from './pages/main';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{title: 'Início'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
